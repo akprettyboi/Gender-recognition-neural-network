a339e574f215f00f6d1b02557b0d11be18af8dca
Ayush Karnawat <98512270+akprettyboi@users.noreply.github.com>
2024-06-16 07:46:36 +0530

    Add files via upload

---
+++ clf_comparison.py
@@ -0,0 +1,54 @@
+"""Compare different classifiers."""
+import warnings
+
+from sklearn.ensemble import RandomForestClassifier, StackingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+
+from data_process import *
+from data_process import visualize
+
+warnings.filterwarnings("ignore")
+
+clf_names = ('Nearest Neighbors', 'SVM', 'Decision Tree', 'Random Forest', 'Neural Net', 'XGBoost', 'Stacked Ensemble') # classifier names
+clfs = (KNeighborsClassifier(n_neighbors=100, weights='distance'), SVC(), DecisionTreeClassifier(),
+        RandomForestClassifier(), MLPClassifier(), XGBClassifier(),
+        StackingClassifier(estimators=[('lr', LogisticRegression()), ('rf', RandomForestClassifier())], final_estimator=LogisticRegression())) # classifier objects
+
+
+def train_clf(x_train, y_train, clf):
+    """
+    Train classifier.
+    :param x_train: Training inputs.
+    :param y_train: Training outputs.
+    :param clf: Untrained classifier object.
+    :return: Trained classifier object.
+    """
+    clf.fit(x_train, y_train)  # train classifier
+
+    return clf
+
+
+def run():
+    """
+    main.
+    :return: None
+    """
+    voice_data = read()  # read data
+    x_train, x_test, y_train, y_test = preprocess(voice_data)  # preprocess data
+
+    for clf_name, clf in zip(clf_names, clfs):  # for all classifiers
+        clf = train_clf(x_train, y_train, clf)  # train classifier
+        print()
+        print(clf_name)
+        get_accuracy(x_train, x_test, y_train, y_test, clf)  # print results
+
+        
+
+
+if __name__ == '__main__':
+    run()
